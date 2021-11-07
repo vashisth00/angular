0edb83defca51f28b5093c798dc152e4095c14c3
Vashisth Bhushan <vb@gounravel.com>
2021-11-07 20:43:31 +0530

    Header props

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  text!: string;
+  @Input()
+  color!: string;
+  constructor() {
+ }
 
   ngOnInit(): void {
   }
