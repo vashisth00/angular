af2d6109b7b64584d2b7226388ca020b732ac342
Vashisth Bhushan <vb@gounravel.com>
2021-11-10 11:05:17 +0530

    New release component

--- src/app/mock-album.ts
+++ src/app/mock-album.ts
@@ -24,14 +24,28 @@ export const NewRelease: Hero[] = [
   },
   {
     image:
-      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
+      'https://ik.imagekit.io/hk/image_2__3__fGWyirJvK-A.png?updatedAt=1636492624714',
     title: 'The Idea Of Her',
     artist: 'Ali Gatie',
     type: 'album'
   },
   {
     image:
-      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
+      'https://ik.imagekit.io/hk/image_2__2__OA2wrn6ivyl.png?updatedAt=1636492623886',
+    title: 'Here',
+    artist: 'Here',
+    type: 'album'
+  },
+  {
+    image:
+      'https://ik.imagekit.io/hk/image_2__2__OA2wrn6ivyl.png?updatedAt=1636492623886',
+    title: 'Here',
+    artist: 'Here',
+    type: 'album'
+  },
+  {
+    image:
+      'https://ik.imagekit.io/hk/image_2__2__OA2wrn6ivyl.png?updatedAt=1636492623886',
     title: 'Here',
     artist: 'Here',
     type: 'album'
