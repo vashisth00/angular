21a58c9b4834ed9f4dee2d018782e56ff8b635c7
Vashisth Bhushan <vb@gounravel.com>
2021-11-10 10:38:52 +0530

    Component

--- src/app/mock-album.ts
+++ src/app/mock-album.ts
@@ -4,62 +4,34 @@ export const NewRelease: Hero[] = [
   {
     image:
       'https://ik.imagekit.io/hk/image_2_zne6EsUHzUy.png?updatedAt=1636489817123',
-    title: 'Here',
-    artist: 'Here',
-    type: 'album'
-  },
-  {
-    image:
-      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
-    title: 'Here',
-    artist: 'Here',
-    type: 'album'
-  },
-  {
-    image:
-      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
-    title: 'Here',
-    artist: 'Here',
-    type: 'album'
-  },
-  {
-    image:
-      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
-    title: 'Here',
-    artist: 'Here',
-    type: 'album'
-  },
-  {
-    image:
-      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
-    title: 'Here',
-    artist: 'Here',
-    type: 'album'
-  },
-  {
-    image:
-      'https://ik.imagekit.io/hk/image_2__3__fGWyirJvK-A.png?updatedAt=1636492624714',
-    title: 'Here',
-    artist: 'Here',
-    type: 'album'
-  },
-  {
-    image:
-      'https://ik.imagekit.io/hk/image_2__2__OA2wrn6ivyl.png?updatedAt=1636492623886',
-    title: 'Here',
-    artist: 'Here',
+    title: 'House Of Balloons',
+    artist: 'The Weekend',
     type: 'album'
   },
   {
     image:
       'https://ik.imagekit.io/hk/image_2__1__cHp0ICkqCJu.png?updatedAt=1636492623119',
-    title: 'Here',
-    artist: 'Here',
+    title: 'El Dorado',
+    artist: '24kGoldn',
+    type: 'playlist'
+  },
+  {
+    image:
+      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
+    title: 'Just the Way',
+    artist: 'Blanco Brown, Parmalee, Bryce Vine',
+    type: 'Single'
+  },
+  {
+    image:
+      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
+    title: 'The Idea Of Her',
+    artist: 'Ali Gatie',
     type: 'album'
   },
   {
     image:
-      'https://ik.imagekit.io/hk/image_2__1__cHp0ICkqCJu.png?updatedAt=1636492623119',
+      'https://ik.imagekit.io/hk/image_2__4__4XjGHkvUDfA.png?updatedAt=1636492625515',
     title: 'Here',
     artist: 'Here',
     type: 'album'
